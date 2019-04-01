436119fd440521467d00e04f8156996a609d8031
glygapon <glygapon@avid.com>
2019-04-01 17:15:59 +0200

    chore(core): init

---
+++ ccc.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+: ${DIALOG_OK=0}
+: ${DIALOG_CANCEL=1}
+: ${DIALOG_ESC=255}
+
+branch=$(git branch)
+status=$(git status)
+
+exec 3>&1
+
+result=$(dialog \
+--title "$branch" \
+--clear \
+--msgbox "$status" 20 60 \
+--menu "Type:" 20 60 10  \
+  "chore"  "Build process or auxiliary tool changes" \
+  "ci" "CI related changes"         \
+  "docs" "Documentation only changes" \
+  "feat" "A new feature" \
+  "fix" "A bug fix" \
+  "perf" "A code change that improves performance" \
+  "refactor" "A code change that neither fixes a bug or adds a feature" \
+  "release" "Create a release commit" \
+  "style" "Markup, white-space, formatting, missing semi-colons..." \
+  "test" "Adding missing tests" \
+--inputbox "Scope. \n\nExample: core, api, deps ..." 20 60  \
+--inputbox "Subject. \n\nExample: Implemented new Core feature" 20 60 \
+2>&1 1>&3)
+
+return_value=$?
+exec 3>&-
+
+clear
+
+case $return_value in
+  $DIALOG_OK)    
+    read type scope subject <<< $(echo ${result})
+    git commit -am "${type}(${scope}): ${subject}";;
+  $DIALOG_CANCEL)
+    echo "Cancel pressed.";;
+  $DIALOG_ESC)
+    echo "ESC pressed.";;
+esac
---
+++ readme.md
@@ -0,0 +1,12 @@
+conventional changelog commit
+==========================
+
+small tool for create right commit
+
+
+## Demo
+![demo](https://raw.githubusercontent.com/YaroslavGaponov/ccc/master/images/demo.gif "demo")
+
+
+## Link
+Conventional changelog [Github](https://github.com/conventional-changelog)
